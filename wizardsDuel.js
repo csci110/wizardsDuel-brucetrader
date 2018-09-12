d8e696a4fbd28c487390e95ebb858c6743028ec5
Bruce Trader <traderb@dewv.edu>
2018-09-12 16:30:54 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -5,6 +5,7 @@ game.preloadImage("marcusSheet.png", 48, 48);
 game.preloadImage("marcusSpellSheet.png", 48, 48);
 game.preloadImage("strangerSheet.png", 48, 48);
 game.preloadImage("fireballSheet.png", 48, 48);
+game.preloadImage("strangerSpellSheet.png", 48, 48);
 
 //set background
 game.setBackground("floor.png");
@@ -23,6 +24,7 @@ class PlayWizard extends Sprite {
         this.defineAnimation("up", 0, 2);
         this.defineAnimation("right", 3, 5);
         this.speedWhenWalking = 100;
+        this.spellCastTime = 0;
     }
     handleDownArrowKey() {
         this.playAnimation("down");
@@ -42,13 +44,18 @@ class PlayWizard extends Sprite {
     }
 
     handleSpacebar() {
-        this.playAnimation("right");
+        let now = game.getTime();
+        if(now - this.spellCastTime >=1){
+            this.playAnimation("right");
         let spell = new Spell();
         spell.x = this.x + 50;
         spell.y = this.y;
         spell.name = "A spell cast by Marcus";
         spell.setImage("marcusSpellSheet.png");
         spell.angle = 0;
+            this.spellCastTime = now;
+        }
+        
     }
 }
 
@@ -62,17 +69,20 @@ class Spell extends Sprite {
         this.defineAnimation("magic", 0, 7);
         this.playAnimation("magic", true);
     }
-    
+
     handleBoundryContact() {
         game.removeSprite(this);
     }
-    
+
     handleCollision(otherSprite) {
-        if(this.getImage() !== otherSprite.getImage()){
-          game.removeSprite(this);
-        new Fireball(otherSprite);  
+        if (this.getImage() !== otherSprite.getImage()) {
+            let verticalOffset = Math.abs(this.y - otherSprite.y);
+            if (verticalOffset < this.height / 2) {
+                game.removeSprite(this);
+                new Fireball(otherSprite);
+            }
         }
-     return false;
+        return false;
     }
 }
 
@@ -90,17 +100,43 @@ class nonPlayWizard extends Sprite {
         this.defineAnimation("down", 6, 8);
         this.defineAnimation("up", 0, 2);
         this.defineAnimation("left", 9, 11);
+        this.playAnimation("down");
     }
     handleGameLoop() {
         if (this.y <= 0) {
             this.y = 0;
             this.angle = 270;
-            this.playAnimation("down", true);
+            this.playAnimation("down");
         }
         if (this.y >= game.displayHeight - this.height) {
             this.y = game.displayHeight - this.height;
             this.angle = 90;
-            this.playAnimation("up", true)
+            this.playAnimation("up");
+        }
+        if(Math.random() < 0.05){
+          this.playAnimation("left");
+        let spell = new Spell();
+        spell.x = this.x - 50;
+        spell.y = this.y;
+        spell.name = "A spell cast by Stranger";
+        spell.setImage("strangerSpellSheet.png");
+        spell.angle = 180;  
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    handleAnimationEnd() {
+        if (this.angle == 90) {
+            this.playAnimation("up");
+        }
+        if (this.angle == 270) {
+            this.playAnimation("down");
         }
     }
 }
@@ -114,12 +150,19 @@ class Fireball extends Sprite {
         this.height = 48;
         this.x = deadSprite.x;
         this.y = deadSprite.y;
-        this.defineAnimation("explotion",0,7);
+        this.defineAnimation("explotion", 0, 7);
         this.playAnimation("explotion");
         game.removeSprite(deadSprite);
     }
-    handleAnimationEnd(){
+    handleAnimationEnd() {
         game.removeSprite(this);
+        if (!game.isActiveSprite(stranger)) {
+            game.end("Congratulations!\n\nMarcus has defeated the mysterious" +
+                "\nstranger in the dark cloak!");
+        }
+        if(!game.isActiveSprite(marcus)){
+            game.end("Marcus is defeated by the mysterious\nstranger in the dark cloak!\n\nBetter luck next time.");
+        }
     }
 }
 
